97b7305d361f2c0aebdedd4f3c35c03376194175
Alexandre Dantas <eu@alexdantas.net>
2014-10-18 13:19:31 -0400

    Made the battery widget check status differently on my netbook and laptop

--- widgets/battery.lua
+++ widgets/battery.lua
@@ -10,6 +10,20 @@
 --
 -- Don't forget to dofile() this file and include this widget on the Wibox
 
+awful = require("awful");
+
+-- My netbooks have a battery ID of "BAT0", while my new
+-- Toshiba laptop have it as "BAT1".
+-- 
+-- Either way, this makes the widget pull stats from
+-- the right one.
+--
+local battery_id = "BAT0"
+if not awful.util.file_readable("/sys/class/power_supply/BAT0")
+then
+   battery_id = "BAT1"
+end
+
 batterywidget = wibox.widget.textbox()
 batterywidget.border_width = 1
 batterywidget.border_color = beautiful.fg_normal
@@ -19,7 +33,7 @@ vicious.register(
    vicious.widgets.bat, -- available widget from vicious
    " <span color='".. beautiful.bg_focus .. "'>$1 <span color='#50EBEC'>$2% $3</span></span> ",
    1,                   -- refresh interval (seconds)
-   "BAT0"               -- optional argument, battery ID
+   battery_id           -- optional argument, battery ID
 )
 
 
@@ -36,8 +50,8 @@ end
 
 local function battery_notification()
 
-   local f_capacity = assert(io.open("/sys/class/power_supply/BAT0/capacity", "r"))
-   local f_status   = assert(io.open("/sys/class/power_supply/BAT0/status", "r"))
+   local f_capacity = assert(io.open("/sys/class/power_supply/" .. battery_id .. "/capacity", "r"))
+   local f_status   = assert(io.open("/sys/class/power_supply/" .. battery_id .. "/status", "r"))
 
    local bat_capacity = tonumber(f_capacity:read("*all"))
    local bat_status   = trim(f_status:read("*all"))
@@ -71,7 +85,7 @@ local function battery_notification()
 end
 
 -- Check every 'n' seconds
-battimer = timer({ timeout = 60 })
+battimer = timer({ timeout = 30 })
 battimer:connect_signal("timeout", battery_notification)
 battimer:start()
 -- }}}
@@ -88,6 +102,6 @@ batteryprogress:set_color("#AECF96")
 -- Y U NO WORK
 --batteryprogress:set_gradient_colors({ "#AECF96", "#88A175", "#FF5656" })
 
---vicious.register(batteryprogress, vicious.widgets.bat, "$2", 61, "BAT0")
+--vicious.register(batteryprogress, vicious.widgets.bat, "$2", 61, battery_id)
 
 -- }}
